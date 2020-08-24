bf848b9f77cd0e8c5788c90c710bceec61958ae8
Abigail2003progra <70088628+Abigail2003progra@users.noreply.github.com>
2020-08-24 02:30:36 -0600

    Create usuario.java

---
+++ usuario.java
@@ -0,0 +1,20 @@
+public class usuario {
+
+
+//PROPIEDADES DE LA CLASE PRODUCTOS
+
+	String userName[] = new String[3];
+	String password[] = new String[3];
+	
+	usuario() {
+		
+		userName [0] = "sagomez";
+		userName [1] = "gomezsa";
+		userName [2] = "cordinadora";
+		
+		password [0] = "abc123";
+		password [1] = "sa123";
+		password [2] = "ab123";
+	}
+			
+}
